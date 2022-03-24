0d515efc8679a0f66172553e006b1cd8e6eb9ac6
DeltaLF <patricktsaikl@gmail.com>
2022-03-24 17:20:39 +0800

    debug

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,7 @@
 module.exports = function catchAsync(fn) {
   return function (req, res, next) {
     fn(req, res, next).catch((e) => {
+      console.log("error:", e);
       next(e);
     });
   };
